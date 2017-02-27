a0483fd28218ada7e87960deeb67a103bd5ddd1a
one-or-two <13728182003@163.com>
2017-02-27 14:05:59 +0800

    加法运算子类

---
+++ 01_Factory/OperationAdd.cs
@@ -0,0 +1,12 @@
+﻿namespace _01_Factory
+{
+    public class OperationAdd:Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            result = NumberA + NumberB;
+            return result;
+        }
+    }
+}
