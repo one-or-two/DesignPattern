b1e74e1a3adf9eb3725ae1adf8a8cbbb115293d0
one-or-two <13728182003@163.com>
2017-02-27 14:08:52 +0800

    简单工厂模式“计算器”

--- 01_Factory/Program.cs
+++ 01_Factory/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 namespace _01_Factory
 {
     class Program
@@ -10,25 +6,27 @@ namespace _01_Factory
         static void Main(string[] args)
         {
             #region +版本1
-            //Console.Write("请输入数字A：");
-            //string A = Console.ReadLine();
-            //Console.Write("请选择运算符号（+、-、*、/）");
-            //string B = Console.ReadLine();
-            //Console.Write("请输入数字B：");
-            //string C = Console.ReadLine();
-            //string D = "";
-            //if (B == "+")
-            //    D = Convert.ToString(Convert.ToDouble(A) + Convert.ToDouble(C));
-            //if (B == "-")
-            //    D = Convert.ToString(Convert.ToDouble(A) - Convert.ToDouble(C));
-            //if (B == "*")
-            //    D = Convert.ToString(Convert.ToDouble(A)*Convert.ToDouble(C));
-            //if (B == "/")
-            //    D = Convert.ToString(Convert.ToDouble(A)/Convert.ToDouble(C));
-            //Console.WriteLine("结果是："+D);
+            /*
+            Console.Write("请输入数字A：");
+            string A = Console.ReadLine();
+            Console.Write("请选择运算符号（+、-、*、/）");
+            string B = Console.ReadLine();
+            Console.Write("请输入数字B：");
+            string C = Console.ReadLine();
+            string D = "";
+            if (B == "+")
+                D = Convert.ToString(Convert.ToDouble(A) + Convert.ToDouble(C));
+            if (B == "-")
+                D = Convert.ToString(Convert.ToDouble(A) - Convert.ToDouble(C));
+            if (B == "*")
+                D = Convert.ToString(Convert.ToDouble(A) * Convert.ToDouble(C));
+            if (B == "/")
+                D = Convert.ToString(Convert.ToDouble(A) / Convert.ToDouble(C));
+            Console.WriteLine("结果是：" + D);
+             */
             #endregion
             #region +版本2
-            try
+           /* try
             {
                 Console.WriteLine("请输入第一个数字：");
                 var inputNumberA = Console.ReadLine();
@@ -58,13 +56,21 @@ namespace _01_Factory
                 Console.WriteLine("结果是：" + computeResult);
                 Console.ReadLine();
             }
-            catch(Exception exception)
+            catch (Exception exception)
             {
-                Console.WriteLine("您的输入有错："+exception.Message);
+                Console.WriteLine("您的输入有错：" + exception.Message);
             }
-           
+            */
             #endregion
             #region +版本3
+
+            Operation oper;
+            oper = OperationFactory.CreateOperatate("+");
+            oper.NumberA = 1;
+            oper.NumberB = 2;
+            double result = oper.GetResult();
+            Console.Write("结果是："+result);
+            Console.ReadKey();
 
             #endregion
         }
