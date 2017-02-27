b58d48f04056451afcd640295b1a233eaec99e3d
one-or-two <13728182003@163.com>
2017-02-27 14:08:14 +0800

    运算工厂类

---
+++ 01_Factory/OperationFactory.cs
@@ -0,0 +1,26 @@
+﻿namespace _01_Factory
+{
+  public  class OperationFactory
+    {
+      public static Operation CreateOperatate(string operate)
+      {
+          Operation oper = null;
+          switch(operate)
+          {
+              case "+":
+                  oper = new OperationAdd();
+                  break;
+              case "-":
+                  oper = new OperationSub();
+                  break;
+              case "*":
+                  oper = new OperationMul();
+                  break;
+              case "/":
+                  oper = new OperationDiv();
+                  break;
+          }
+          return oper;
+      }
+    }
+}
