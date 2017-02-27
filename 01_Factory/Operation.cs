36864e1ac47279f0063e2c474375068ac9e70960
one-or-two <13728182003@163.com>
2017-02-27 14:05:06 +0800

    运算类

---
+++ 01_Factory/Operation.cs
@@ -0,0 +1,23 @@
+﻿namespace _01_Factory
+{
+   public class Operation
+   {
+       private double _numberA = 0;
+       private double _numberB = 0;
+       public double  NumberA
+       {
+           get { return _numberA; }
+           set { _numberA = value; }
+       }
+       public double NumberB
+       {
+           get { return _numberB; }
+           set { _numberB = value; }
+       }
+       public virtual double GetResult()
+       {
+           double result = 0;
+           return result;
+       }
+   }
+}
