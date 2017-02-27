6b6bc3689237496c982c5786e88de2176d482f19
one-or-two <13728182003@163.com>
2017-02-27 14:07:47 +0800

    除法运算子类

---
+++ 01_Factory/OperationDiv.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01_Factory
+{
+  public class OperationDiv:Operation
+    {
+      public override double GetResult()
+      {
+          double result = 0;
+          if(NumberB==0)
+              throw new Exception("除数不能为0.");
+            result = NumberA/NumberB;
+          return result;
+      }
+    }
+}
